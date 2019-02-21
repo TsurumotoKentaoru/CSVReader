fd31d86fbf9ec5063ce88f74e1f0b52f6f04e74a
Tsuruken <tsurumotokentarou@gmail.com>
2019-02-21 21:58:11 +0900

    初回コミット

---
+++ CSVReader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CSVReader
+{
+    public static string[][] getCSV(string filePath)
+    {
+        TextAsset csvFile = Resources.Load(filePath) as TextAsset;
+        if (csvFile == null)
+        {
+            return null;
+        }
+        StringReader reader = new StringReader(csvFile.text);
+        List<string[]> csvDatas = new List<string[]>();
+        if (reader != null)
+        {
+            while (reader.Peek() > -1)
+            {
+                string line = reader.ReadLine();
+                csvDatas.Add(line.Split(','));
+            }
+            return csvDatas.ToArray();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
